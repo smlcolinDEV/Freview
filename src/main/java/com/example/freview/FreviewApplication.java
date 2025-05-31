b92198dee688bf2a2e389877df6670413a99708d
Motodies <39949690+Motodies@users.noreply.github.com>
2025-05-31 11:36:45 +0200

    Setup Database

---
+++ src/main/java/com/example/freview/FreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.freview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freview/FreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
